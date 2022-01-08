fbb013fd629c4647357fb012eda8fef973bab5f6
jpmoraess <moraesdeveloperweb@gmail.com>
2022-01-08 15:57:11 -0300

    Comportamentos de escrita do Map

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,9 @@
 fun main() {
-    val pedidos = mapOf<Int, Double>(Pair(1, 2.0), Pair(2, 34.20), 3 to 25.34) // to -> infix function
+    val pedidos = mutableMapOf<Int, Double>(
+        Pair(1, 2.0),
+        Pair(2, 34.20),
+        3 to 25.34
+    ) // to -> infix function
 
     // quando a performance for necessária, usar infix function pode não ser uma boa opção...
 
@@ -11,11 +15,32 @@ fun main() {
         println("pedido: $it")
     }
 
-    for (pedido: Map.Entry<Int, Double> in pedidos) {
+    for (p: Map.Entry<Int, Double> in pedidos) {
         println("--------------------------------")
-        println("número do pedido: ${pedido.key}")
-        println("valor do pedido: ${pedido.value}")
+        println("número do pedido: ${p.key}")
+        println("valor do pedido: ${p.value}")
     }
+
+    /**
+     * Trabalhando com escrita...
+     */
+
+    pedidos[4] = 36.20
+    println(pedidos)
+    pedidos.put(5, 80.20)
+    println(pedidos)
+    pedidos[5] = 29.30
+    println(pedidos)
+    pedidos.putIfAbsent(6, 320.20) // putIfAbsent só adiciona caso não tenha
+    println(pedidos)
+    pedidos.putIfAbsent(6, 509.20)// putIfAbsent só adiciona caso não tenha
+
+    pedidos.remove(6)
+    println(pedidos)
+    pedidos.remove(3)
+    println(pedidos)
+    pedidos.remove(1, 2.0) // a remoção passando o 2° parametro só ocorre quando o valor informado for correspondente...
+    println(pedidos)
 }
 
 
